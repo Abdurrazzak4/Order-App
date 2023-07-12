6a66e5e5a173a7b18bef1ff9e73c1d98991ee8c3
Abdurrazzak Saymaz <abdurrazzak1905@gmail.com>
2023-07-12 21:34:57 +0300

    updated

--- OrderApp/index.js
+++ OrderApp/index.js
@@ -1,7 +1,10 @@
-import { menuArray } from '/data.js'
+import { menuArray } from '/OrderApp/data.js'
 
 const main = document.getElementById("main")
+const orderList = document.getElementById("order-list")
 
+
+function render(){
 menuArray.forEach(menuItem => {
     main.innerHTML += `
     <div id="menu-item" >
@@ -11,7 +14,21 @@ menuArray.forEach(menuItem => {
                     <div  id="diner-includes">${menuItem.ingredients}</div>
                     <div  id="diner-price">$${menuItem.price}</div>
                 </div>
-                <button id="add-item">+</button>
+                <button id="${menuItem.id}">+</button>
             </div>
     `
 });
+}
+render()
+
+document.addEventListener("click", (e)=>{
+    if(e.target.id === menuItem.id){
+        addOrder(e.target.id)
+        
+    }
+})
+
+
+function addOrder(e){
+    orderList.classList.toggle("hidden")
+}
