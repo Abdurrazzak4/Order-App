d3a03209dd541d5fd6bcf73f711592224dcccbe1
Abdurrazzak Saymaz <abdurrazzak1905@gmail.com>
2023-07-13 13:16:24 +0300

    upd

--- OrderApp/index.js
+++ OrderApp/index.js
@@ -1,8 +1,11 @@
-import { menuArray } from '/data.js'
+import { menuArray } from "/OrderApp/data.js";
 
-const main = document.getElementById("main")
+const main = document.getElementById("main");
+const orderList = document.getElementById("order-list");
+const totalPrice = document.getElementById("total-price");
 
-menuArray.forEach(menuItem => {
+function render() {
+  menuArray.forEach((menuItem) => {
     main.innerHTML += `
     <div id="menu-item" >
                 <div id="img">${menuItem.emoji}</div>
@@ -11,7 +14,52 @@ menuArray.forEach(menuItem => {
                     <div  id="diner-includes">${menuItem.ingredients}</div>
                     <div  id="diner-price">$${menuItem.price}</div>
                 </div>
-                <button id="add-item">+</button>
+                <button id="btn" data-id="${menuItem.id}">+</button>
             </div>
+    `;
+  });
+}
+render();
+
+document.addEventListener("click", (e)=>{
+    if(e.target.id === "btn")
+    handleBtnClicked(e.target.dataset.id)
+    if(e.target.tagName === "SPAN"){
+        e.target.parentElement.parentElement.remove()
+        totalPrices(0)
+    }
+})
+const orders = []
+function handleBtnClicked(id){
+    const matchedId = menuArray.filter((menu)=>{
+       return (menu.id === parseInt(id)); 
+    })[0]
+    addOrder(matchedId)
+    orders.push(matchedId)
+    
+    
+    console.log(orders);
+}
+
+function addOrder(menuItem){
+    orderList.innerHTML += `
+    
+      <div id="order-menu">
+        <div id="added-items">${menuItem.name} <span>remove</span></div>
+        <div id="added-price">$${menuItem.price}</div>
+      </div>
+
     `
-});
+    totalPrices(menuItem.price)
+}
+
+function totalPrices(price) {
+let total = price;
+for (let i = 0; i < orders.length; i++) {
+  total += orders[i].price;
+}
+
+totalPrice.innerHTML = `
+    <div id="total">Total: $${total}</div>
+`
+}
